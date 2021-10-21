0908729f4c3614b99710e61cfe21be73e0771fbd
MichuTopeczka <72551175+MichuTopeczka@users.noreply.github.com>
2021-10-21 20:16:34 +0200

    Add files via upload

--- es/hello.js
+++ es/hello.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
-});
+exports.area = function(width) { return width * width; };
+exports.perimeter = function(width) { return 4 * width; };
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}!`)
-}); s
+const square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
+console.log('The area of a square with a width of 4 is ' + square.area(4));
+
+module.exports = {
+   area: function(width) {
+     return width * width;
+   },
+ 
+   perimeter: function(width) {
+     return 4 * width;
+   }
+ };
